ab2592bc7f2d0bee38a1bdfdae3431aa02d1a55c
allan paris <allanparis8@gmail.com>
2026-05-19 10:17:48 +0200

    add exception dateCorrupted and InvalidQuest

---
+++ exception/DataCorruptedException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class DataCorruptedException extends Exception {
+    public DataCorruptedException(String message) {
+        super(message);
+    }
+}
---
+++ exception/InvalidQuestException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class InvalidQuestException extends Exception {
+    public InvalidQuestException(String message) {
+        super(message);
+
+    }
+}
